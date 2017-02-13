7170b4e63cf7ebf611c2c35f50660312e5f3728d
Frank Warnaar <f.warnaar@viriciti.com>
2017-02-13 14:54:52 +0100

    spotify ajax calls

--- week2/assets/js/app.js
+++ week2/assets/js/app.js
@@ -1,12 +1,91 @@
 /*jshint esversion: 6 */
 (function() {
     'use strict';
-    
-    const config = {};
+
+    const config = {
+        api: {
+            url: 'https://api.spotify.com/v1/search',
+        },
+        search: {
+            form: document.getElementsByTagName('form')[0],
+            input: document.querySelector('input[type=search]')
+        }
+    };
 
     const app = {
         init() {
-            routes.init();
+            search.init();
+        }
+    };
+
+    const search = {
+        init() {
+            config.search.form.addEventListener('submit', this.handle);
+        },
+        handle(e) {
+            // Prevent browser from refreshing
+            e.preventDefault();
+
+            const searchQuery = config.search.input.value;
+            const requestUrl = `${config.api.url}?q=${searchQuery}&type=track,artist`;
+            const request = new XMLHttpRequest();
+
+            request.open('GET', requestUrl, true);
+
+            request.onload = () => {
+                if (request.status >= 200 && request.status < 400) {
+                    const data = JSON.parse(request.responseText);
+                    view.render(cleanLists.artists(data.artists.items), cleanLists.tracks(data.tracks.items));
+                } else {
+                    console.log('error');
+                }
+            };
+
+            request.onerror = err => {
+                console.log(err);
+            };
+
+            request.send();
+        }
+    };
+
+    const cleanLists = {
+        artists(artists) {
+            artists = artists.map(artist => {
+                return {
+                    id: artist.id,
+                    name: artist.name,
+                    images: artist.images,
+                };
+            });
+            return artists;
+        },
+        tracks(tracks) {
+            tracks = tracks.map(track => {
+                return {
+                    id: track.id,
+                    name: track.name,
+                    artists: this.getArtistsNamesOfTrack(track.artists),
+                    images: track.album.images
+                };
+            });
+            return tracks;
+        },
+        getArtistsNamesOfTrack(artists) {
+            artists = artists.map(artist => {
+                return {
+                    id: artist.id,
+                    name: artist.name
+                };
+            });
+            return artists;
+        }
+    };
+
+    const view = {
+        render(artists, tracks) {
+            console.log(artists);
+            console.log(tracks);
         }
     };
 
--- week2/dist/js/app.js
+++ week2/dist/js/app.js
@@ -4,11 +4,92 @@
 (function () {
     'use strict';
 
-    var config = {};
+    var config = {
+        api: {
+            url: 'https://api.spotify.com/v1/search'
+        },
+        search: {
+            form: document.getElementsByTagName('form')[0],
+            input: document.querySelector('input[type=search]')
+        }
+    };
 
     var app = {
         init: function init() {
-            routes.init();
+            search.init();
+        }
+    };
+
+    var search = {
+        init: function init() {
+            config.search.form.addEventListener('submit', this.handle);
+        },
+        handle: function handle(e) {
+            // Prevent browser from refreshing
+            e.preventDefault();
+
+            var searchQuery = config.search.input.value;
+            var requestUrl = config.api.url + '?q=' + searchQuery + '&type=track,artist';
+            var request = new XMLHttpRequest();
+
+            request.open('GET', requestUrl, true);
+
+            request.onload = function () {
+                if (request.status >= 200 && request.status < 400) {
+                    var data = JSON.parse(request.responseText);
+                    view.render(cleanLists.artists(data.artists.items), cleanLists.tracks(data.tracks.items));
+                } else {
+                    console.log('error');
+                }
+            };
+
+            request.onerror = function (err) {
+                console.log(err);
+            };
+
+            request.send();
+        }
+    };
+
+    var cleanLists = {
+        artists: function artists(_artists) {
+            _artists = _artists.map(function (artist) {
+                return {
+                    id: artist.id,
+                    name: artist.name,
+                    images: artist.images
+                };
+            });
+            return _artists;
+        },
+        tracks: function tracks(_tracks) {
+            var _this = this;
+
+            _tracks = _tracks.map(function (track) {
+                return {
+                    id: track.id,
+                    name: track.name,
+                    artists: _this.getArtistsNamesOfTrack(track.artists),
+                    images: track.album.images
+                };
+            });
+            return _tracks;
+        },
+        getArtistsNamesOfTrack: function getArtistsNamesOfTrack(artists) {
+            artists = artists.map(function (artist) {
+                return {
+                    id: artist.id,
+                    name: artist.name
+                };
+            });
+            return artists;
+        }
+    };
+
+    var view = {
+        render: function render(artists, tracks) {
+            console.log(artists);
+            console.log(tracks);
         }
     };
 
